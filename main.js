73040ae5928c96bff1ccf3b0361856965e21ed7d
SimoTr84 <simone.trucco@gmail.com>
2017-07-12 17:33:33 +1000

    files set up and html organised

---
+++ main.js
@@ -0,0 +1,16 @@
+function solution() {
+  // define the DOM elements and store them in different variables
+  // validate the DOM elements "first_name" and "last_name" to contain some character, they can't be empty
+  // validate the DOM element "email" to contain one @ character, before and after have to contain letters and numbers and dots. Cannot be empty
+  // validate the if company is checked, then validate the elements "company_name" (cannot be empty) and "phone" (it can't be empty consisting of at least six digits, can contain dashes and spaces)
+}
+
+// write your code in JavaScript (Node.js 6.4.0)
+//
+// you can access DOM Tree using DOM Object Model:
+//    document.getElementById
+// or using jQuery:
+//    $('some_tag')
+//
+// you can write to stdout for debugging purposes, e.g.
+// console.log('this is a debug message');
